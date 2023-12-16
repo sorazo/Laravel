e1ea79d920715d8cf4a8f4a2623595f18a844f8f
fukuhara_miyu <m.fukuhara@lull-inc.co.jp>
2023-12-16 17:16:49 +0900

    authorsとbooksのモデル・コントローラー・マイグレーション・シード

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(AuthorsTableSeeder::class);
+        $this->call(BooksTableSeeder::class);
     }
 }
